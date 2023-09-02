0fb0f0eb8534d1663fadb136962d0eb5b0d026d8
Mac Tirney <mactirney@gmail.com>
2023-09-01 21:58:16 -0400

    Create data file for firebase

---
+++ backend/models/student.js
@@ -0,0 +1,19 @@
+const Student =  {
+    name: '',
+    email: '',
+    classes: [
+        {
+            className: '',
+            teacher: '',
+            assignments: [
+                {
+                    assignmentName: '',
+                    grade: 0
+                }
+            ],
+            classGrade: 0
+        }
+    ]
+};
+
+module.exports = Student;
